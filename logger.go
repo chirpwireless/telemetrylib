aff0996fb2484b2d3c61a137184781459e734898
evgeny.mironenko <evgeny.mironenko@chirpwireless.io>
2024-09-11 22:02:14 +0200

    Extend the otel function to create a tracer provider. Create a wrapper around slog to support trace and span ids

---
+++ logger.go
@@ -0,0 +1,68 @@
+package telemetrylib
+
+import (
+	"context"
+	"go.opentelemetry.io/otel/trace"
+	"log/slog"
+	"os"
+)
+
+func InitLogger() {
+	jsonHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: replacer,
+		Level:       slog.LevelInfo,
+	})
+	// Add span context attributes when Context is passed to logging calls.
+	instrumentedHandler := handlerWithSpanContext(jsonHandler)
+	// Set this handler as the global slog handler.
+	slog.SetDefault(slog.New(instrumentedHandler))
+}
+
+func handlerWithSpanContext(handler slog.Handler) *spanContextLogHandler {
+	return &spanContextLogHandler{Handler: handler}
+}
+
+// spanContextLogHandler is an slog.Handler which adds attributes from the
+// span context.
+type spanContextLogHandler struct {
+	slog.Handler
+}
+
+// Handle overrides slog.Handler's Handle method. This adds attributes from the
+// span context to the slog.Record.
+func (t *spanContextLogHandler) Handle(ctx context.Context, record slog.Record) error {
+	// Get the SpanContext from the golang Context.
+	spanContext := trace.SpanContextFromContext(ctx)
+	if spanContext.IsValid() {
+		// Add trace context attributes following Cloud Logging structured log format described
+		// in https://cloud.google.com/logging/docs/structured-logging#special-payload-fields
+		record.AddAttrs(
+			slog.Any("logging.googleapis.com/trace", spanContext.TraceID()),
+		)
+		record.AddAttrs(
+			slog.Any("logging.googleapis.com/spanId", spanContext.SpanID()),
+		)
+		record.AddAttrs(
+			slog.Bool("logging.googleapis.com/trace_sampled", spanContext.TraceFlags().IsSampled()),
+		)
+	}
+	return t.Handler.Handle(ctx, record)
+}
+
+func replacer(groups []string, a slog.Attr) slog.Attr {
+	// Rename attribute keys to match Cloud Logging structured log format
+	switch a.Key {
+	case slog.LevelKey:
+		a.Key = "severity"
+		// Map slog.Level string values to Cloud Logging LogSeverity
+		// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+		if level := a.Value.Any().(slog.Level); level == slog.LevelWarn {
+			a.Value = slog.StringValue("WARNING")
+		}
+	case slog.TimeKey:
+		a.Key = "timestamp"
+	case slog.MessageKey:
+		a.Key = "message"
+	}
+	return a
+}
